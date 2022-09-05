efe3d346e85a56c2a19ec738944c1823222632be
TashviGulati <109961072+TashviGulati@users.noreply.github.com>
2022-09-05 16:49:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var images = ["https://i.postimg.cc/MGn9GJXw/family.jpg","person1.jpg","person2.jpg","person3.jpg","person4.png","person5.jpg"];
+var names = ["Fmaily Book","Ranbir Singh", "Diljeet Singh", "Rocky Singh", "Alia Singh", "Soni Singh"];
+var i = 0;
+function update()
+{
+    i++;
+    var numbers_of_family_member_in_array = 5
+    if(i > numbers_of_family_member_in_array)
+      {
+          i = 0;
+      }
+    
+     
+    var updatedImage = images[i];
+    //Debug the code to store list of names in updatedName. Use names[i]
+    var updatedName = names[i] ;
+ 
+    document.getElementById("family_member_image").src = updatedImage;
+    document.getElementById("family_member_name").innerHTML = updatedName;
+}
